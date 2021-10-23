dbf4732645d2a8a4b6b49439bc21559e83c9512e
Dilan_105464 <dilan.fernando@axiatadigitallabs.com>
2021-10-23 18:05:52 +0530

    Initial commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+    console.log(this.tasks);
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter((t) => t.id !== task.id ));
+  }
+
+  toggleRemainder(task: Task) {
+    task.remainder = !task.remainder;
+    this.taskService.updateTask(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Meeting at school',
+        day: 'May 6th at 1:30pm',
+        remainder: true
+    },
+    {
+        id: 2,
+        text: 'Highlevel road',
+        day: 'May 7th at 1:30pm',
+        remainder: true
+    },
+    {
+        id: 3,
+        text: 'Gas station',
+        day: 'May 9th at 1:30pm',
+        remainder: true
+    },
+    {
+        id: 4,
+        text: 'Appointment',
+        day: 'May 16th at 1:30pm',
+        remainder: false
+    }
+]
